578df04351bab31019ee818c20cce24dd3a1ef2d
Nikita L <hexlite24@gmail.com>
2023-11-18 00:42:34 +0200

    reply to message in telegraph plugin

--- telegraph/tasks.py
+++ telegraph/tasks.py
@@ -9,12 +9,18 @@ from .helper import TelegraphDownloader
 
 
 class TelegraphScrapeTask(Task):
-    def run(self, links: list[dict[str, str]], *args) -> AppResult:
+    def run(self, links: list[dict[str, str]], msgdoc_data: dict) -> None:
         result = AppResult()
         for link in links:
             downloader = TelegraphDownloader(link)
             downloader.download()
             result.merge(downloader.status)
-        asyncio.run(push_message_notification(Notification(text="TG DL finished")))
-        return result
 
+        asyncio.run(
+            push_message_notification(
+                Notification(
+                    text=result and "TG DL finished succesfully" or "TG DL failed",
+                    reply_to_message_id=msgdoc_data["message_id"]
+                )
+            )
+        )
